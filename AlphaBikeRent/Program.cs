cd2922135603d9d7fc364c0c08dbfe143ad2ac3a
Erla Edvadsdottir <erlama85@gmail.com>
2018-09-17 14:51:38 +0200

    Add project files.

---
+++ AlphaBikeRent/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlphaBikeRent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
