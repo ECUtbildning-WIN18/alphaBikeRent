b5a1e69ed0bd59105c413fb4cf3250179d2a556d
Eva Persson <persson_eva@outlook.com>
2018-09-21 10:34:27 +0200

    Menu

---
+++ AlphaBikeRent/Domain/Menu.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlphaBikeRent.Domain
+{
+    class Menu
+    {
+        public Menu()
+        {
+
+        }
+        public void RunningMenu()
+        {
+            Console.WriteLine("--------Welcome to Alpha Bikerental--------");
+            int usersChoice = 0;
+            do
+            {
+                bool loop = true;
+                do
+                {
+                    Console.WriteLine(" ");
+                    Console.WriteLine("Choose by following options");
+                    Console.WriteLine("Renting a ordinary bike, press 1 ");
+                    Console.WriteLine("Renting a electric bike, press 2");
+                    Console.WriteLine("for xxxx, press 3 ");
+                    Console.WriteLine("Press 0 to exit the application");
+                    Console.Write(">>");
+
+                    try
+                    {
+                        usersChoice = int.Parse(Console.ReadLine());
+                        loop = false;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("You are only allowed to type numbers between 0-3");
+                    }
+                } while (loop);
+
+                switch (usersChoice)
+                {
+                    case 1: //RentOrdinaryBike();
+                        break;
+
+                    case 2: //RentElectricBike():
+                        break;
+                    case 3: //DoSomething();
+                        break;
+                    case 0:
+                        Console.WriteLine("Welcome back!");
+                        break;
+
+                }
+
+            } while (usersChoice != 0);
+
+        }
+    }
+}
--- AlphaBikeRent/Program.cs
+++ AlphaBikeRent/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using AlphaBikeRent.Domain;
 
 namespace AlphaBikeRent
+
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            Menu menu = new Menu();
+
+            menu.RunningMenu();
+
         }
     }
 }
